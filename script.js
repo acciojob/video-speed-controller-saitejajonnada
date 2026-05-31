45d4f38f5df5d6d8f39dbae923a13c146d8ee740
Jonnada Sai Teja <146433770+saitejajonnada@users.noreply.github.com>
2026-05-31 16:06:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,62 @@
-const inputs = document.querySelectorAll('.controls input');
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressFilled = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const sliders = player.querySelectorAll('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+// Play / Pause
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
     }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+//// Change button icon
+function updateButton() {
+    toggle.textContent = video.paused ? '►' : '❚ ❚';
+}
+
+//// Update progress bar
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressFilled.style.flexBasis = `${percent}%`;
+}
+
+////Seek video
+function scrub(e) {
+    const scrubTime =
+        (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+//// Volume & Playback Speed
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+//// Skip buttons
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+//// Event Listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+sliders.forEach(slider =>
+    slider.addEventListener('input', handleRangeUpdate)
+);
+
+skipButtons.forEach(button =>
+    button.addEventListener('click', skip)
+);
+
+progress.addEventListener('click', scrub);
